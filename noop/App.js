c8d5e90adc5b62a4e45b010feeb3116ebcb69b29
이진우 <khl2bb@naver.com>
2020-08-31 23:49:47 +0900

    일단 완성

--- noop/App.js
+++ noop/App.js
@@ -1,51 +1,66 @@
-import React from 'react';
-import LoadingScreen from "./Loading"
-import * as Location from 'expo-location';
-import { render } from 'react-dom';
-import { Alert } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import LoadingScreen from "./Loading";
+import * as Location from "expo-location";
+import { render } from "react-dom";
+import { Alert } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import axios from "axios";
-
-const API_KEY = "102ba13c4b713a6ad4f11e93fd061f72";
+import Weather from "./Weather";
 
 export default class extends React.Component {
-  state = {
-    isLoading: true
-  };
-  
-  getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
-    );
-    console.log(data);
-    // this.setState({
-    //   isLoading: false,
-    //   condition: weather[0].main,
-    //   temp
-    // });
-  };
+	state = {
+		isLoading: true,
+		temp: 999,
+		condition: "Clear",
+	};
 
-  getLocation = async() => {
-    try {
-      // throw Error();
-      await Location.requestPermissionsAsync();
-      const { coords:{ latitude, longitude } } = await Location.getCurrentPositionAsync();
-      this.getWeather(latitude, longitude)
-      this.setState({ isLoading: false });
-    } catch (error) {
-      Alert.alert("위치를 찾지 못 하겠습니다.", "슬프네요.");
-      this.setState( { isLoading:true } );
-    }
-    
-  };
+	getWeather = async (latitude, longitude) => {
+		const {
+			data: {
+				main: { temp },
+				weather,
+			},
+		} = await axios.get(
+			`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+		);
+		this.setState({
+			isLoading: false,
+			temp,
+			condition: weather[0].main,
+		});
+		// console.log(data.main);
+		// this.setState({
+		//   isLoading: false,
+		//   condition: weather[0].main,
+		//   temp
+		// });
+	};
 
-  componentDidMount() {
-    this.getLocation();
-  };
+	getLocation = async () => {
+		try {
+			// throw Error();
+			await Location.requestPermissionsAsync();
+			const {
+				coords: { latitude, longitude },
+			} = await Location.getCurrentPositionAsync();
+			this.getWeather(latitude, longitude);
+			this.setState({ isLoading: false });
+		} catch (error) {
+			Alert.alert("위치를 찾지 못 하겠습니다.", "슬프네요.");
+			this.setState({ isLoading: true });
+		}
+	};
 
-  render() {
-    const { isLoading } = this.state;
-    return isLoading ? <LoadingScreen /> : <Text>찾았다!</Text>;
-  };
-  
+	componentDidMount() {
+		this.getLocation();
+	}
+
+	render() {
+		const { isLoading, temp, condition } = this.state;
+		return isLoading ? (
+			<LoadingScreen />
+		) : (
+			<Weather 온도={temp} 상태={condition} />
+		);
+	}
 }
--- noop/Loading.js
+++ noop/Loading.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function LoadingScreen() {
-    return (
-        <View style={styles.container}>
-          <Text style={styles.text}>로딩 중</Text>
-          <StatusBar style="dark" />
-        </View>
-      );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>로딩 중</Text>
+			<StatusBar style="dark" />
+		</View>
+	);
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#3498db',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-    },
-    text: {
-      flex:0.55,
-    //   backgroundColor:"black",
-      color: 'white',
-      fontSize:50,
-      fontWeight:"bold",
-    },
-  });
+	container: {
+		flex: 1,
+		backgroundColor: "#3498db",
+		alignItems: "center",
+		justifyContent: "flex-end",
+	},
+	text: {
+		flex: 0.55,
+		//   backgroundColor:"black",
+		color: "white",
+		fontSize: 50,
+		fontWeight: "bold",
+	},
+});
