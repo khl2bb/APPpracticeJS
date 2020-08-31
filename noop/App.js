e0f6c27eeea03d84eb1579530a88e579e48914f5
이진우 <khl2bb@naver.com>
2020-08-31 19:17:44 +0900

    로딩 화면 만들기

--- noop/App.js
+++ noop/App.js
@@ -1,34 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import LoadingScreen from "./Loading"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.secondBox}>
-        <Text style={styles.text}>안녕 나는 APP.JS 야</Text>
-      </View>
-      <Text style={styles.text}>안녕 나는 APP.JS 야</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <LoadingScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  secondBox: {
-    flex:3,
-    backgroundColor: 'yellow',
-
-  },
-  text: {
-    flex:0.5,
-    backgroundColor: 'bisque',
-    color: 'white',
-  },
-});
---
+++ noop/Loading.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+
+export default function LoadingScreen() {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.text}>로딩 중</Text>
+          <StatusBar style="dark" />
+        </View>
+      );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#3498db',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+    },
+    text: {
+      flex:0.55,
+    //   backgroundColor:"black",
+      color: 'white',
+      fontSize:50,
+      fontWeight:"bold",
+    },
+  });
